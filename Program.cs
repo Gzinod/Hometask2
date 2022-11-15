0c9e1aae961619e9f6365c0d78117448cd2a77e8
GzINOD <gzinod31@rambler.ru>
2022-11-15 19:03:55 +0300

    Homework

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("a is bigger then b");
+}
+else Console.WriteLine("b is bigger then a");
